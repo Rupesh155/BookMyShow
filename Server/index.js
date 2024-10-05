f4f9f50f4a65f76a4ea7cfbd99f23e6c10d848a6
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-10-05 19:00:22 +0530

    otp

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,22 @@
- let express=  require('express')
-  let app=   express()
+const express = require('express');
+const sendOtp = require('./twillioService'); 
+const app = express();
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('heheh')
+app.post('/send-otp', async (req, res) => {
+  const { phoneNumber } = req.body;
 
-})
 
-  app.listen(5000,()=>{
-    console.log('server running on port no 5k');
-    
-  })
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
+  try {
+    await sendOtp(phoneNumber, otp);
+    res.status(200).send({ message: 'OTP sent successfully', otp });
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to send OTP' });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ Server/twillioService.js
@@ -0,0 +1,22 @@
+// twilioService.js
+const twilio = require('twilio');
+
+// Use your Twilio Account SID and Auth Token
+const accountSid = 'ACdc793fa8d3863de168a371a541ca057a'; // Your Account SID from Twilio Console
+const authToken = '33d14fbf349ed16f8884822d99ebf664';   // Your Auth Token from Twilio Console
+
+const client = new twilio(accountSid, authToken);
+
+const sendOtp = (phoneNumber, otp) => {
+  return client.messages.create({
+    body: `Your OTP is ${otp}`,  // The message content
+    from: '+19514195099', // Your Twilio Phone Number
+    to: phoneNumber                // Recipient's Phone Number
+  });
+};
+
+module.exports = sendOtp;
+
+
+
+
